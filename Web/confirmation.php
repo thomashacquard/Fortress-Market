0957cfa88eb83008d59bdfec02407d52f184dd49
amatokus8669 <amatokus8669@users.noreply.github.com>
2017-04-09 14:24:46 +0200

    Add files via upload

---
+++ Web/confirmation.php
@@ -0,0 +1,17 @@
+﻿<?php session_start();?>
+
+<!DOCTYPE HTML>
+<html>
+<head> 
+<link  rel="stylesheet" type="text/css" href="stylepage.css"> 
+</head>
+<body>
+
+<div class="textbox" style="text-align:center;"> 
+	<p>Votre compte a bien été créé!</p>
+	<a href="connexion.php">Veuillez vous connecter ici</a>
+</div>
+
+
+</body>
+</html>
---
+++ Web/connexion.php
@@ -0,0 +1,40 @@
+﻿<?php session_start();?>
+
+<!DOCTYPE HTML>
+<html>
+<head> 
+<link  rel="stylesheet" type="text/css" href="stylepage.css"> 
+</head>
+<body>
+
+<div class="textbox"> 
+<form action="" method="post">
+Identifiant: <input type="text" name="id" class="champ"><br>
+Mot De Passe: <input type="password" name="mdp" class="champ"><br>
+<input type="submit" name="submitbutton" id="valider">
+</form>
+</div>
+
+<?php
+if(isset($_POST['submitbutton'])){ //check if form was submitted
+$ndc =  $_POST['id'];
+$mdp = $_POST['mdp'];
+
+$comptes = file_get_contents("comptes.json");
+$valcomptes = json_decode($comptes);
+
+if(isset($valcomptes->$ndc)){
+	if($mdp == $valcomptes->$ndc->mdp){
+		$_SESSION['utilisateur'] = $ndc;
+		$_SESSION['mail'] = $valcomptes->$ndc->mail;
+		exit();
+	}else{
+	echo '<script> alert("Mot de passe invalide"); </script>';
+	}
+}else{
+	echo '<script> alert("Nom de compte invalide"); </script>';
+}
+}
+?>
+</body>
+</html>
