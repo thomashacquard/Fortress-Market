6b0280d5568b9181e7790c5f8bc73c1d71526231
amatokus8669 <amatokus8669@users.noreply.github.com>
2017-04-15 23:26:56 +0200

    Ajout de 2 fichiers php
    Le premier: Pour l'actualisation des données qui se fait désormais en php.
    
    Le second: La page ou est redirigé le client s'il n'est pas identifié.

---
+++ Web/alerte.php
@@ -0,0 +1,19 @@
+﻿<?php session_start();?>
+
+<!DOCTYPE HTML>
+<html>
+<head> 
+<link  rel="stylesheet" type="text/css" href="stylepage.css"> 
+</head>
+<body>
+
+<div class="textbox" style="text-align:center;"> 
+	<h3>Vous ne vous êtes pas identifié!</h3>
+	<a href="inscription.php">Inscrivez vous</a>
+	<p>ou</p>
+	<a href="connexion.php">Connectez vous</a>
+</div>
+
+
+</body>
+</html>
