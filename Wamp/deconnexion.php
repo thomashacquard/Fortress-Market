9da076f1e86ff4b25d29752397838e3b408d36c1
amatokus8669 <amatokus8669@users.noreply.github.com>
2017-05-12 09:50:42 +0200

    Add files via upload

--- Wamp/deconnexion.php
+++ Wamp/deconnexion.php
@@ -2,5 +2,4 @@
 session_start();
 session_destroy();
 header("Location: /Fortress%20Market/connexion.php");
-exit();
 ?>
--- Wamp/inscription.php
+++ Wamp/inscription.php
@@ -1,39 +1,43 @@
-<?php session_start();?>
-
+<?php
+session_start();
+?>
 <!DOCTYPE HTML>
 <html>
 <head> 
 <link  rel="stylesheet" type="text/css" href="css/stylepage.css"> 
+<link  rel="stylesheet" type="text/css" href="Footer/footer.css"> 
+<meta charset="UTF-8">
 </head>
 <body>
 
-<div class="textbox"> <!--Balise générique de type block-->
-
-<form action="" method="post">	<!--La balise form est utilisée pour créer un formulaire-->
+<div class="textbox"> 
+<form action="" method="post">
 	Identifiant: <input type="text" name="id" class="champ"><br>
 	E-Mail: <input type="text" name="mail" class="champ"><br>
 	Mot De Passe: <input type="password" name="mdp" class="champ"><br>
 	Vérification du Mot De Passe: <input type="password" name="mdpverif" class="champ"><br>
-	<input type="submit" name="submitbutton" id="valider">	<!--c'est le bouton valider-->
+	<input type="submit" name="submitbutton" id="valider">
 </form>
+</br>
+<a href="connexion.php" style="font-size: 12px; margin-left: 40px;">Déjà inscrit? connectez-vous ici!</a>
 </div>
 
 <?php
-if(isset($_POST['submitbutton'])){	//la variable doit être définie et est différente de 0 //on passe a la suite si le bouton a été activé
-	$id = $_POST['id'];	 //on attribue les nouvelles valeurs de la variable a 
+if(isset($_POST['submitbutton'])){//check if form was submitted
+	$id = $_POST['id'];
 	$mail = $_POST['mail'];
 	$mdp = $_POST['mdp'];
 	$mdpverif = $_POST['mdpverif'];
-	if(empty($id) || empty($mail) || empty($mdp) || empty($mdpverif)){	//les cases ne doivent pas être vides
-		echo '<script> alert("Veuillez remplir tous les champs."); </script>';	//sinon un message d'erreur s'affiche
+	if(empty($id) || empty($mail) || empty($mdp) || empty($mdpverif)){
+		echo '<script> alert("Veuillez remplir tous les champs."); </script>';
 	}else{
-		if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){	//on filtre l'adresse mail saisie pour s'assurer qu'elle soit valide
+		if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
 			echo '<script> alert("Email invalide!"); </script>';
-		}elseif($_POST['mdp'] != $_POST['mdpverif']) {	//si la valeur du mdp est différente que sa valeur rentrée précédemment
+		}elseif($_POST['mdp'] != $_POST['mdpverif']) {
 			echo '<script> alert("Les mots de passe ne sont pas similaires"); </script>';
 		}else{
-				$comptes = file_get_contents("Data/comptes.json");
-			if(isset(json_decode($comptes)->$id)){ //on verifie que le compte n'existe pas déjà
+				$comptes = file_get_contents("Data/comptes.json"); 
+			if(isset(json_decode($comptes)->$id)){
 				echo '<script> alert("Un compte lié à cet identifiant existe déjà!"); </script>';
 			}else{
 				$valcomptes = json_decode($comptes,true);
@@ -45,11 +49,10 @@ if(isset($_POST['submitbutton'])){	//la variable doit être définie et est diff
 				fopen("Data/Users/".$id.".json", "w");
 				fclose();
 				header("Location: /Fortress%20Market/confirmation.php");
-				exit();
 			}
 		}
 	}
 }
-?>
+include 'Footer/footer.php';?>
 </body>
 </html>
